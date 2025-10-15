ccc66caf7fed94f777c41df05709b56edcd68e47
zandoh <zclarkmail@gmail.com>
2025-10-14 22:32:27 -0400

    feat: migrate to tsup from unbuild

---
+++ tsup.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  splitting: false,
+  treeshake: true,
+  minify: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  sourcemap: false,
+  target: 'es2022',
+  external: [],
+  outDir: 'dist',
+  env: {
+    NODE_ENV: 'production',
+  },
+  esbuildOptions(options) {
+    options.legalComments = 'none'
+    options.platform = 'neutral'
+  },
+})
